8df2db92aef057f37c9bcdb44cfd2704ef85a619
Daniel Kats <dbkats@gmail.com>
2015-02-15 15:34:52 -0500

    removed dead code

--- ece1779-a1-email.py
+++ ece1779-a1-email.py
@@ -22,31 +22,6 @@ logger = logging.getLogger("ece1779-a1-email")
 coloredlogs.install(show_timestamps=False, show_hostname=False)
 ######################################
 
-
-
-def read_group_ids(fname, used_fname):
-    pattern = "CREATE USER 'group(\d+)' IDENTIFIED BY '(\d+)';"
-    d = {}
-    with open(fname) as fp:
-        for line in fp:
-            m = re.match(pattern, line)
-            if m:
-                d[ int(m.group(1)) ] = int(m.group(2))
-    with open(used_fname) as fp:
-        used_groups = set([int(line.split(" ")[1].strip()) for line in fp if line.strip()])
-    for group_id in used_groups:
-        del d[group_id]
-    return d
-
-def read_amazon_ids(fname, used_fname):
-    with open(fname) as fp:
-        ids = [line.strip() for line in fp]
-    with open(used_fname) as fp:
-        used_ids = [line.split(" ")[1].strip() for line in fp if line.strip()]
-    for id in used_ids:
-        ids.remove(id)
-    return ids
-
 def seek_to_group(buf):
     text = cStringIO.StringIO(buf)
     line = text.readline()
@@ -102,11 +77,6 @@ def extract_group_info(text, num_members):
         group.append(m)
     return group
 
-def read_submitted_ids(fname):
-    with open(fname) as fp:
-        reg = [line.strip() for line in fp]
-    return reg
-
 def read_email(email_text):
     obj = {}
     email_text = string.translate(email_text, None, "[]?*(),!&^%%$#+=/\\|{}~`:;'\"_")
@@ -115,43 +85,6 @@ def read_email(email_text):
     obj['num_members'] = extract_num_members(text)
     obj['students'] = extract_group_info(text, obj['num_members'])
     return obj
-
-def write_student(student_num):
-    with open("students.txt", "a") as fp:
-        fp.write("%s\n" % student_num)
-
-def register_group(obj):
-    group_logins = read_group_ids("groups.txt", "used_groups.txt")
-    prev_reg = read_submitted_ids("students.txt")
-    amazon_ids = read_amazon_ids("amazon_ids.txt", "amazon_used.txt")
-    amazon_mappings = {}
-    for student in obj['students']:
-        if student['student_num'] in prev_reg:
-            assert False, "Student number %s already registered for an Amazon key" % student['student_num']
-        else:
-            amazon_mappings[student['student_num']] = amazon_ids.pop()
-    if obj['group_num'] == UNDEF_GROUP or obj['group_num'] not in group_logins:
-        obj['group_num'] = max(group_logins.keys())
-    else:
-        obj['group_num'] = int(obj['group_num'])
-    assert obj['group_num'] in group_logins, "Group number is invalid"
-    database_pwd = group_logins[obj['group_num']]
-
-    for student_num, aws_key in amazon_mappings.iteritems():
-        # write that these are now used
-        write_used_amazon_key(student_num, aws_key)
-        write_student(student_num)
-    write_used_group(obj)
-    return amazon_mappings, database_pwd
-
-def write_used_amazon_key(student_num, aws_key):
-    with open("amazon_used.txt", "a") as fp:
-        fp.write("%s %s\n" % (student_num, aws_key))
-
-def write_used_group(obj):
-    with open("used_groups.txt", "a") as fp:
-        for student in obj['students']:
-            fp.write("%s %s\n" % (student['student_num'], obj['group_num']))
 
 def respond_to_email(obj):
     emails = {}
